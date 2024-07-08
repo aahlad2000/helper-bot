6c4d8551a9830301be984a01f5008e3d7f7054e1
Aahlad <aahladkethineeedi@gmail.com>
2024-07-08 23:45:22 +0530

    initial commit

---
+++ src/main/java/com/discord/helper_bot/HelperBotApplication.java
@@ -0,0 +1,13 @@
+package com.discord.helper_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discord/helper_bot/HelperBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discord.helper_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
